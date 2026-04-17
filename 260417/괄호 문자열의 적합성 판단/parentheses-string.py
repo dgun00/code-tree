0362aa85ccb8ab4a904469525fd2792aead326fb
한동건 <127089776+dgun00@users.noreply.github.com>
2026-04-17 11:47:24 +0900

    Title: 괄호 문자열의 적합성 판단, Time: 0ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260417/괄호 문자열의 적합성 판단/parentheses-string.py
@@ -0,0 +1,45 @@
+str = input()
+
+# Please write your code here.
+
+class stack:
+    def __init__(self):
+        self.stk = []
+    
+    def push(self,e):
+        self.stk.append(e)
+
+    def pop(self):
+        poped = self.stk.pop()
+        return poped
+
+    def empty(self):
+        if len(self.stk) == 0:
+            return 1
+        else: return 0
+    
+    def top(self):
+        return self.stk(len(self.stk)-1)
+
+stk = stack()
+
+def f(stk, str):
+    
+    for e in str:
+        if e == '(':
+            stk.push(e)
+        
+        if e == ')':
+            if stk.empty():
+                res = "No"
+            
+            else: stk.pop()
+
+    if stk.empty():
+        res= "Yes"
+    else:
+        res = "No"
+
+    print(res)
+
+f(stk,str)

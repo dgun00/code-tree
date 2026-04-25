7e28723ff6764e033efff84896a051a198cd0ca5
한동건 <127089776+dgun00@users.noreply.github.com>
2026-04-25 11:54:24 +0900

    Title: 스승의 은혜 3, Time: 0ms, Memory: 0MB, Status: Wrong Answer - Codetree

--- 260425/스승의 은혜 3/the-grace-form-teacher-3.py
+++ 260425/스승의 은혜 3/the-grace-form-teacher-3.py
@@ -4,15 +4,14 @@ P = [gift[0] for gift in gifts]
 S = [gift[1] for gift in gifts]
 
 # Please write your code here.
-arr = []
+
 total =0
 sorted = sorted(gifts, key=lambda x: x[0]/2 + x[1])
-
 for p,s in gifts:
     total += p + s
 
 idx = N-1
-while total >B:
+while 1:
     if (total - (sorted[idx][0]/2 + sorted[idx][1])) > B:
         total = (total - (sorted[idx][0] + sorted[idx][1]))
         idx -= 1

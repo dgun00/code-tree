5024b6c664f72ff5aaa93e71db47923e33526dbb
한동건 <127089776+dgun00@users.noreply.github.com>
2026-05-18 11:43:57 +0900

    [Passed] 최소 점프 횟수 (46ms, 16MB)

--- 260518/최소 점프 횟수/min-num-of-jumps.py
+++ 260518/최소 점프 횟수/min-num-of-jumps.py
@@ -4,28 +4,26 @@ num = list(map(int, input().split()))
 # Please write your code here.
 
 min_res = 99999
-res = 0
-flag = 0
-def is_in_range(n):
-    return n < N 
 
-def jump(n):
-    global res
+flag = 0
+ 
+
+def jump(idx,cnt):
+
     global flag
     global min_res
-    if N == n+1:
-        min_res = min(min_res,res)
+
+    if idx >=N-1 :
+        min_res = min(min_res,cnt)
         flag = 1
         return
     
-    for i in range(1,num[n]+1):
-        if is_in_range(n+i):
-            res +=1
-            
-            jump(n+i)
-            res -=1
+    for i in range(1,num[idx]+1):
+        jump(idx+i,cnt+1)
 
-jump(0)
+
+jump(0,0)
+
 if flag == 1:
     print(min_res)
 else:

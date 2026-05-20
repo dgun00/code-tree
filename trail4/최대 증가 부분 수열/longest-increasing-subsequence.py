b590447ac0c988399d66b5cf8e051b31a9546fc5
한동건 <127089776+dgun00@users.noreply.github.com>
2026-05-20 16:47:43 +0900

    [Passed] 최대 증가 부분 수열 (79ms, 17MB)

---
+++ trail4/최대 증가 부분 수열/longest-increasing-subsequence.py
@@ -0,0 +1,23 @@
+import sys
+n = int(input())
+m = list(map(int, input().split()))
+
+# Please write your code here.
+
+dp = [1]*n
+
+# dp[0] = 1
+
+max_val = 1
+
+for i in range(1,n):
+        for j in range(i-1,-1,-1):
+                if m[j] < m[i]:
+                        max_val = max(max_val, dp[j]+1)
+        
+        dp[i] = max_val
+        max_val = 1
+
+print(max(dp))
+
+

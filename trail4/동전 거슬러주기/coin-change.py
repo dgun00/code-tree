3823f11f44264c49f0d19fee87b2b4ce6b1c4ba8
한동건 <127089776+dgun00@users.noreply.github.com>
2026-05-20 22:34:28 +0900

    [Passed] 동전 거슬러주기 (54ms, 17MB)

---
+++ trail4/동전 거슬러주기/coin-change.py
@@ -0,0 +1,30 @@
+N, M = map(int, input().split())
+coin = list(map(int, input().split()))
+
+# Please write your code here.
+
+dp = [10001]* (M+1)
+
+dp[0] = 0
+
+coin.sort(reverse=True)
+
+
+
+for i in range(1,M+1):
+    # if dp[i] != -1:
+    #     continue
+    
+    for v in coin:
+        if v > i:
+            continue
+        elif v==i:
+            dp[i] = 1
+            continue
+
+        dp[i] = min(dp[i],dp[i-v] + 1)
+
+# print(dp)
+if dp[M]== 10001:
+    dp[M] = -1
+print(dp[M])
